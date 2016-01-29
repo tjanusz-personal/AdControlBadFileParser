99daf27bda44f934c594a9487c3940d7d0018759
Tim Janusz <tjanusz@pointroll.com>
2016-01-29 15:54:13 -0500

    Updating to include CLK, TRK, ADP, ONP, CCB server info

--- prod_drive_connect.rb
+++ prod_drive_connect.rb
@@ -12,11 +12,11 @@ RESOURCETYPE_DISK         = 0x1
 RESOURCEDISPLAYTYPE_SHARE = 0x3
 RESOURCEUSAGE_CONNECTABLE = 0x1
 
-add_connection = Win32API.new('mpr', 'WNetAddConnection2', 'PPPP', 'I')
+ADD_CONNECTION = Win32API.new('mpr', 'WNetAddConnection2', 'PPPP', 'I')
 
 # read user name/password from command line  domain/username password
-user_name = ARGV[0]
-user_password = ARGV[1]
+USER_NAME = ARGV[0]
+USER_PASSWORD = ARGV[1]
 
 def build_netresource(remote_name)
   netresource_struct = [
@@ -32,15 +32,39 @@ def build_netresource(remote_name)
   netresource_struct
 end
 
+def connect_to_remote(server_prefix, server_list)
+  server_list.each do |server_name|
+    remote_name = "\\\\#{server_prefix}-#{server_name}.pointroll.local"
+    netresource_struct = build_netresource(remote_name)
+    return_value = ADD_CONNECTION.call(
+      netresource_struct.pack('LLLLPPPP'),    # lpNetResource
+      USER_PASSWORD, USER_NAME, nil                       # dwFlags
+    )
+    puts "connecting to #{remote_name} with return value: #{return_value}"
+  end
+end
+
+
 # TODO: figure out how to make this dynamic
 server_list = ["WEB06", "WEB07", "WEB08", "WEB09", "WEB10", "WEB11", "WEB12", "WEB13", "WEB14", "WEB15", "WEB16", "WEB17",
   "WEB18", "WEB19", "WEB20", "WEB21", "WEB22", "WEB23", "WEB24", "WEB25", "WEB26", "WEB27", "WEB28", "WEB29"]
-server_list.each do |server_name|
-  remote_name = "\\\\P-PR-ADS-#{server_name}.pointroll.local"
-  netresource_struct = build_netresource(remote_name)
-  return_value = add_connection.call(
-    netresource_struct.pack('LLLLPPPP'),    # lpNetResource
-    user_password, user_name, nil                       # dwFlags
-  )
-  puts "connecting to #{remote_name} with return value: #{return_value}"
-end
+connect_to_remote("P-PR-ADS", server_list)
+
+track_list = ["WEB21", "WEB22", "WEB23", "WEB24", "WEB25", "WEB26", "WEB27", "WEB28", "WEB29", "WEB30", "WEB31", "WEB32",
+  "WEB33", "WEB34", "WEB35", "WEB36", "WEB37", "WEB38", "WEB39", "WEB40"]
+connect_to_remote("P-PR-TRK", track_list)
+
+click_list = ["WEB11", "WEB12", "WEB13", "WEB14" ]
+connect_to_remote("P-PR-CLK", click_list)
+
+util_list = ["DS01"]
+connect_to_remote("P-PR-UTL", util_list)
+
+adp_list = ["WEB21", "WEB22", "WEB23", "WEB24"]
+connect_to_remote("P-PR-ADP", adp_list)
+
+onp_list = ["WEB11", "WEB12", "WEB13", "WEB14"]
+connect_to_remote("P-PR-ONP", onp_list)
+
+ccb_list = ["WEB11", "WEB12"]
+connect_to_remote("P-PR-CCB", ccb_list)
--- prod_drive_size.rb
+++ prod_drive_size.rb
@@ -16,13 +16,73 @@ def get_disk_free_space(path)
       return 0
     end
     l,h = free_caller.unpack("II")
-    l + (h << 32)
+    result = l + (h << 32)
+    (result/1073741824.0).round(2)
+end
+
+def print_disk_sizes(server_prefix, server_list, c_min, d_min, include_d = true)
+  server_list.each do |server_name|
+    d_path = "\\\\#{server_prefix}-#{server_name}.pointroll.local\\D$"
+    c_path = "\\\\#{server_prefix}-#{server_name}.pointroll.local\\C$"
+    if include_d
+      d_space = get_disk_free_space(d_path)
+    else
+      d_space = "N/A"
+    end
+    c_space = get_disk_free_space(c_path)
+    if c_space < c_min
+      c_space = "#{red(c_space)}"
+    end
+
+    if d_space != "N/A" and d_space < d_min
+      d_space = "#{red(d_space)}"
+    end
+
+    puts "Server: #{server_prefix}-#{server_name} \t C: #{c_space} \t D: #{d_space}"
+  end
+end
+
+def colorize(text, color_code)
+  "\e[#{color_code}m#{text}\e[0m"
+end
+
+def red(text)
+  colorize(text, 31)
 end
 
 # TODO: need to figure out how to make this dynamic
+puts "### AD SERVERS \t\t C: 9 \t\t D: 100"
 server_list = ["WEB06", "WEB07", "WEB08", "WEB09", "WEB10", "WEB11", "WEB12", "WEB13", "WEB14", "WEB15", "WEB16", "WEB17",
   "WEB18", "WEB19", "WEB20", "WEB21", "WEB22", "WEB23", "WEB24", "WEB25", "WEB26", "WEB27", "WEB28", "WEB29"]
-server_list.each do |server_name|
-  path = "\\\\P-PR-ADS-#{server_name}.pointroll.local\\D$"
-  puts "Server: #{path} free space: #{(get_disk_free_space(path)/1073741824.0).round(2)} GB"
-end
+print_disk_sizes("P-PR-ADS", server_list, 9, 100)
+
+puts ""
+puts "### TRACK SERVERS \t C: 8 \t\t D: 60"
+track_list = ["WEB21", "WEB22", "WEB23", "WEB24", "WEB25", "WEB26", "WEB27", "WEB28", "WEB29", "WEB30", "WEB31", "WEB32",
+  "WEB33", "WEB34", "WEB35", "WEB36", "WEB37", "WEB38", "WEB39", "WEB40"]
+print_disk_sizes("P-PR-TRK", track_list, 8, 60)
+
+puts ""
+puts "### CLICK SERVERS \t C: 20 \t\t D: 49"
+click_list = ["WEB11", "WEB12", "WEB13", "WEB14"]
+print_disk_sizes("P-PR-CLK", click_list, 20, 49)
+
+puts ""
+puts "### UTL SERVERS \t C: 12 \t\t D:40"
+util_list = ["DS01"]
+print_disk_sizes("P-PR-UTL", util_list, 12, 40)
+
+puts ""
+puts "### ONP SERVERS \t C: 23 \t\t D: 40"
+onp_list = ["WEB11", "WEB12", "WEB13", "WEB14"]
+print_disk_sizes("P-PR-ONP", onp_list, 23, 40)
+
+puts ""
+puts "### ADP SERVERS \t C: 18"
+adp_list = ["WEB21", "WEB22", "WEB23", "WEB24"]
+print_disk_sizes("P-PR-ADP", adp_list, 18, 0, false)
+
+puts ""
+puts "### CCB SERVERS \t C: 25"
+ccb_list = ["WEB11", "WEB12"]
+print_disk_sizes("P-PR-CCB", ccb_list, 25, 0, false)
